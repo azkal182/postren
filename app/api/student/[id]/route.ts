557d70bdaad85b43441b292b9be3020dcd6949d8
macbbook <macbbook@macbbooks-MacBook-Pro.local>
2024-08-25 23:01:58 +0700

    add api student

--- app/api/student/[id]/route.ts
+++ app/api/student/[id]/route.ts
@@ -2,6 +2,9 @@ import { db } from '../../../../lib/db';
 
 export async function GET(request: Request, { params }: { params: { id: string } }) {
     const id = params.id; // 'a', 'b', or 'c'
+    if (!id) {
+        return Response.json({ error: 'field id required!' });
+    }
     const student = await db.master.findMany({
         where: {
             studentId: id,
